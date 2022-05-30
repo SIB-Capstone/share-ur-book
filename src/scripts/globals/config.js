1320aa015857077a7d0b28fef2ea2573d1f358df
Mas 'Edi <aslekmak54@gmail.com>
2022-05-30 11:18:14 +0700

    add

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,12 @@
+/* eslint-disable indent */
+const CONFIG = {
+    BASE_URL: '',
+    BASE_IMAGE_URL: '',
+    CACHE_NAME: new Date().toISOString(),
+    // CACHE_NAME: 'share-ur-book',
+    DATABASE_NAME: 'share-ur-book',
+    DATABASE_VERSION: 1,
+    OBJECT_STORE_NAME: 'shareBooks',
+};
+
+export default CONFIG;
