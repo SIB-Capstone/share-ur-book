f9940d22f96251738e060d3e23e013026c12f49a
Mas 'Edi <aslekmak54@gmail.com>
2022-06-04 13:22:09 +0700

    little change book template layout

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,10 +1,10 @@
 const CONFIG = {
   KEY: 'AIzaSyCQfYAItHUZlTt6hC8xvBW1hzcZjBsz-ek',
   BASE_URL: 'https://www.googleapis.com/books/v1/volumes',
-   // CACHE_NAME: 'share-ur-book',
-    DATABASE_NAME: 'share-ur-book',
-    DATABASE_VERSION: 1,
-    OBJECT_STORE_NAME: 'shareBooks',
+  // CACHE_NAME: 'share-ur-book',
+  DATABASE_NAME: 'share-ur-book',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'shareBooks',
 };
 
 export default CONFIG;
