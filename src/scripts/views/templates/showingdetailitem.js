34e9c5b293642eafd986c78e77855902be3df72c
Saputra Langgang <saputra.langgang.nara@gmail.com>
2022-06-30 00:53:50 +0700

    add review and delete review fitur

--- src/scripts/views/templates/showingdetailitem.js
+++ src/scripts/views/templates/showingdetailitem.js
@@ -1,4 +1,3 @@
-/* eslint-disable indent */
 class Showingdetailitem {
     static showingdetailitem(books) {
         const { volumeInfo } = books;
@@ -19,7 +18,7 @@ class Showingdetailitem {
         <div class="container detail-card">
         <div class="row">
             <div class="col-lg-6 col-sm-12" id="imageBook">
-            <img src="${imageLinks.thumbnail}"/>
+            <img src="${imageLinks.thumbnail}" alt="booksImage"/>
             </div>
             <div class="col-lg-6 col-sm-12" id="detailBook">
             <h5>${title}</h5>
@@ -35,5 +34,3 @@ class Showingdetailitem {
 }
 
 export default Showingdetailitem;
-
-// codingan sementara. belum jalan
